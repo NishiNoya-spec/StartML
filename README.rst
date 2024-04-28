2c59f534e5e1c76caf77c9f61d226e326e8601dc
NishiNoya-spec <danil.temnokhudov00@mail.ru>
2024-04-28 23:02:35 +0700

    Last changes for 28.04.2024

--- README.rst
+++ README.rst
@@ -16,7 +16,8 @@ Postman
 
 pip install -Ur .\requirements.txt --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host=files.pythonhosted.org
 
-
+import sys
+sys.path.append('E:/Рабочий стол С/python_projects/carpov_courses_python')
 
 
 
--- Разработка на Python/less_12/src/app.py
+++ Разработка на Python/less_12/src/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Depends
 import psycopg2
+import yaml
 import uvicorn
 from psycopg2.extras import RealDictCursor
 import os
 from dotenv import load_dotenv
-
+from src.crud import get_likes, get_feed
+from pathlib import Path
 
 app = FastAPI()
 
@@ -24,15 +26,25 @@ def get_db():
         conn.close()
 
 
-@app.get("/user")
-def get_all_users(limit: int = 10, db = Depends(get_db)):
+def config():
+    print(__file__)
+    with open(Path(__file__).parent.parent / "params.yaml", "r") as f:
+        return yaml.safe_load(f)
+
+
+@app.get("/user/feed")
+def get_all_users(user_id: int, limit: int = 10, db = Depends(get_db), config: dict = Depends(config)):
     with db.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute(
-            f"""
-            SELECT *
-            FROM "user"
-            LIMIT {limit}
             """
+            SELECT *
+            FROM feed_action
+            WHERE user_id = %(user_id)s
+                AND time >= %(start_date)s
+            ORDER BY time
+            LIMIT %(limit_user)s
+            """,
+            {"user_id": user_id, "limit_user": limit, "start_date": config["feed_start_date"]},
         )
         return cursor.fetchall()
 
